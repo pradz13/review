a1b49912d58db9e155457fee597167b2dc04a52a
Pradipta <pradipta.nag2@gmail.com>
2023-07-29 22:21:00 +0530

    Call the external api to fetch the articles per category - WIP

--- review-ui/src/components/article/ListArticles.js
+++ review-ui/src/components/article/ListArticles.js
@@ -1,10 +1,27 @@
 import { useLocation } from "react-router-dom";
+import Header from "../header/Header";
+import React, { useEffect, useState } from "react";
 
 function ListArticles() {
   const location = useLocation();
 
+  const [response, setResponse] = useState();
+
+  const getApiData = async () => {
+    const response = await fetch(
+      "http://localhost:9001/review-that-matters/articles/categories/MOVIE_REVIEW/0"
+    ).then((response) => response.json());
+    setResponse(response);
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
+
   return (
     <>
+      <Header />
       <h1>{location.state.articleCategory}</h1>
     </>
   );
